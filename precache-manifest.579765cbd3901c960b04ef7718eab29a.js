e59137521dad51471cf4b3c74dea16b5ad3168d4
Gedas <gediminas.braskus@gmail.com>
2020-06-26 16:36:08 +0300

    Updates

---
+++ precache-manifest.579765cbd3901c960b04ef7718eab29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3700eb52c9c8e766c33ad4af50ac872e",
+    "url": "/exercise_1/index.html"
+  },
+  {
+    "revision": "b7d9155e693a9cc60764",
+    "url": "/exercise_1/static/css/main.7573bd1e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/exercise_1/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exercise_1/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d9155e693a9cc60764",
+    "url": "/exercise_1/static/js/main.96bfa583.chunk.js"
+  },
+  {
+    "revision": "f98a73f9cd71990defa4",
+    "url": "/exercise_1/static/js/runtime-main.b5cec801.js"
+  }
+]);
